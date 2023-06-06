8d5fba7bc1c4f1f13dee484953351e67efe0d1d1
Eva Zaadnoordijk <evazaadnoordijk@gmail.com>
2023-06-06 11:30:41 +0200

    added detailpage

--- script/script.js
+++ script/script.js
@@ -1,20 +1,6 @@
-// import { errorState } from "../modules/error.js";
-// import { loadingState } from "../modules/loading.js";
-
 // variables
 const deNav = document.querySelector("nav");
 const menuKnop = document.querySelector("header nav>a");
-
-// loadingState();
-
-// fetchData()
-//   .then((data) => {
-//     renderProfile(data);
-//     console.log(data);
-//   })
-//   .catch((error) => {
-//     errorState();
-//   });
 
 // eventlisteners
 menuKnop.addEventListener(
